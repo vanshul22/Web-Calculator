bab7e0198483280621035870aad8552033afa0a2
Vanshul Kesharwani <vkvanshulkesharwani54@gmail.com>
2022-04-05 13:08:46 +0530

    Version 1.0.0 : Initial commit.

---
+++ JS/script.js
@@ -0,0 +1,8 @@
+/*
+Website Name - Web-Calculator
+Name : Vanshul Kesharwani
+Date : 05/04/2022
+Version : 1.0.0
+Email : vkvanshulkesharwani54@gmail.com
+Description : This is a Web calculator application for performing different types of calculations.this project works without refreshing page in javaScript.
+*/
---
+++ README.md
@@ -0,0 +1,4 @@
+# Web-Calculator
+This is a Web calculator application for performing different types of calculations.this project works without refreshing page in javaScript.
+
+To check live page please [Click Here](https://vanshul22.github.io/Web-Calculator/).
