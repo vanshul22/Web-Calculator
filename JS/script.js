492e42c034939579d0faa7676d6eddde4c47099d
Vanshul Kesharwani <vkvanshulkesharwani54@gmail.com>
2022-04-05 16:51:57 +0530

    Version 2.1.1 : Added Table in HTML file.

--- JS/script.js
+++ JS/script.js
@@ -2,7 +2,7 @@
 Website Name - Web-Calculator
 Name : Vanshul Kesharwani
 Date : 05/04/2022
-Version : 1.0.0
+Version : 2.1.1
 Email : vkvanshulkesharwani54@gmail.com
 Description : This is a Web calculator application for performing different types of calculations.this project works without refreshing page in javaScript.
 */
